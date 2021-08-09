9df5d12845b75bf339b012bdf688a28f89c93a6b
RosiePlatten <r.v.platten@gmail.com>
2021-08-09 18:40:20 +0100

    added rainbow mode

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let mode ="basic"
-let gridSize = 16;
-console.log("hello");
+let mode ="basic";
 getPageElements();
-setUpGrid(gridSize,mode);
+setUpGrid(16,mode);
 
 function getPageElements(){
     canvas = document.querySelector('#canvas');
     clearButton = document.querySelector('#clearButton');
     sizeSlider = document.querySelector('#sizeSlider');
     sizeLabel = document.querySelector('#sizeLabel');
+    gradientButton = document.querySelector("#gradientButton");
+    rainbowButton = document.querySelector("#rainbowButton");
+    classicButton = document.querySelector("#basicButton");
 }
 function setUpGrid(gridSize,mode){
     canvas.style.display = "grid";
@@ -19,15 +20,25 @@ function setUpGrid(gridSize,mode){
         for(let r=0; r<gridSize; r++){
             let gridSquare = document.createElement('div');
             gridSquare.addEventListener("mouseover",function(){
-                this.style.backgroundColor=getPenColor(mode);
+                this.style.backgroundColor=getPenColor(this);
             });
             canvas.appendChild(gridSquare);
             gridSquares.push(gridSquare);
         }
     }
 }
-function getPenColor(mode){
-    return "black";
+function getPenColor(gridSquare){
+    switch (mode){
+        case "basic":
+            return "black";
+            break;
+        case "rainbow":
+            return generateRainbowColor();
+            break;
+        case "gradient":
+            return generateGradientColor(gridSquare);
+            break;
+    }
 }
 clearButton.addEventListener('click',function(){
     for(let i=0; i<gridSquares.length; i++){
@@ -46,3 +57,19 @@ function deleteGridSquares(){
     }
 }
 
+rainbowButton.addEventListener("click", function(){
+    mode="rainbow";
+})
+gradientButton.addEventListener("click", function(){
+    mode="gradient";
+})
+classicButton.addEventListener("click", function(){
+    mode="basic";
+})
+ function generateRainbowColor(){
+     return "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")";
+ }
+function generateGradientColor(gridSquare){
+    let currentColor = gridSquare.style.backgroundColor;
+    return "black";
+}
